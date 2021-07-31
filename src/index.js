2bc999f9c8d6f860a70677c372442e7841c9a423
Daniel Melchor <danielmelchor@localhost.localdomain>
2021-07-31 12:07:43 +0200

    trying shib response w session

--- src/index.js
+++ src/index.js
@@ -13,11 +13,81 @@ app.use(express.json());
 app.set("port", process.env.PORT || 8000);
 
 // Authentication
-const { passport } = require("./auth/auth");
+const passport = require("passport");
+const SamlStrategy = require("passport-saml").Strategy;
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+const SamlOptions = {
+  // URL that goes from the Identity Provider -> Service Provider
+  callbackUrl: process.env.CALLBACK_URL,
+  // URL that goes from the Service Provider -> Identity Provider
+  entryPoint: process.env.ENTRY_POINT,
+  // Usually specified as `/shibboleth` from site root
+  issuer: process.env.ISSUER,
+  identifierFormat: null,
+  validateInResponseTo: false,
+  disableRequestedAuthnContext: true,
+};
+
+SamlOptions.decryptionPvk = JSON.parse(`"${process.env.SHIBBOLETH_KEY}"`);
+SamlOptions.privateCert = JSON.parse(`"${process.env.SHIBBOLETH_KEY}"`);
+SamlOptions.cert = JSON.parse(`"${process.env.SHIBBOLETH_IDP_CERT}"`);
+
+const samlStrategy = new SamlStrategy(SamlOptions, (profile, done) =>
+  done(null, profile)
+);
+
+passport.use(samlStrategy);
+
+// Authentication for routes
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  else return res.redirect("/login");
+};
+
+const session = require("express-session");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
+app.use(passport.session());
+
+// Authentication routes
+app.get(
+  "/login/",
+  passport.authenticate("saml", { failureRedirect: "/login/fail" }),
+  (req, res) => res.redirect("/")
+);
+
+app.post(
+  "/login/callback",
+  passport.authenticate("saml", { failureRedirect: "/login/fail" }),
+  (req, res) => res.redirect("/")
+);
+
+app.get("/login/fail", (req, res) => res.status(401).send("Login failed"));
+
+app.get("/login/metadata", (req, res) => {
+  res.type("application/xml");
+  let cert = JSON.parse(`"${process.env.SHIBBOLETH_CERT}"`);
+
+  res
+    .status(200)
+    .send(samlStrategy.generateServiceProviderMetadata(cert, cert));
+});
 
 // Routes
-const authRouter = require("./auth/router");
 // const coursesRouter = require("./routers/courses");
 // const calendarsRouter = require("./routers/calendars");
 // const hubsRouter = require("./routers/hubs");
@@ -25,7 +95,6 @@ const authRouter = require("./auth/router");
 // const professorsRouter = require("./routers/professors");
 // const ratingsRouter = require("./routers/ratings");
 
-app.use("/login", authRouter);
 // app.use("/courses", coursesRouter);
 // app.use("/calendars", calendarsRouter);
 // app.use("/hubs", hubsRouter);
